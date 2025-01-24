c4e181b8f8db129cc4121be9e07a8901a7851843
Daniyar Kudaibergenev <Daniyar.Kudaibergenev@kcell.kz>
2025-01-25 03:02:44 +0500

    model and controller changes 8

--- src/RestGenServiceProvider.php
+++ src/RestGenServiceProvider.php
@@ -1,32 +1,13 @@
-<?php
-
-namespace Olivion\RestGen;
+<?php namespace Olivion\RestGen;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class RestGenServiceProvider extends ServiceProvider
 {
     protected $commands = [
         'RestTest' => 'command.rest.test',
     ];
-
-    /** 
-     * Perform post-registration booting of services.
-     *
-     * @return void
-     */
-    public function boot(): void
-    {
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'olivion');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'olivion');
-        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
-
-        // Publishing is only necessary when using the CLI.
-        if ($this->app->runningInConsole()) {
-            $this->bootForConsole();
-        }
-    }
 
     /** 
      * Register any package services.
@@ -36,12 +17,12 @@ class RestGenServiceProvider extends ServiceProvider
     public function register(): void
     {
         
-        $this->mergeConfigFrom(__DIR__.'/../config/rest-gen.php', 'rest-gen');
+        // $this->mergeConfigFrom(__DIR__.'/../config/rest-gen.php', 'rest-gen');
 
-        // Register the service the package provides.
-        $this->app->singleton('rest-gen', function ($app) {
-            return new RestGen;
-        });
+        // // Register the service the package provides.
+        // $this->app->singleton('rest-gen', function ($app) {
+        //     return new RestGen;
+        // });
 
         $this->registerCommands($this->commands);
     }
@@ -71,6 +52,40 @@ class RestGenServiceProvider extends ServiceProvider
             return new Console\RestTestCommand();
         });
     }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array_values($this->commands);
+    }
+
+    /** 
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'olivion');
+        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'olivion');
+        // $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        // $this->loadRoutesFrom(__DIR__.'/routes.php');
+
+        // Publishing is only necessary when using the CLI.
+        // if ($this->app->runningInConsole()) {
+        //     $this->bootForConsole();
+        // }
+    }
+
+    
+
+    
+
+    
 
     /**
      * Get the services provided by the provider.
